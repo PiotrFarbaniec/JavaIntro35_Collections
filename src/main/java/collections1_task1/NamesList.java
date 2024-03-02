b93ee70993343729ec4229b3a75b48421f7e8b9d
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-03-02 08:56:32 +0100

    Collections1--task1_commit03

--- src/main/java/collections1_task1/NamesList.java
+++ src/main/java/collections1_task1/NamesList.java
@@ -21,36 +21,28 @@ public class NamesList {
         list3.addAll(namesList);
 
         System.out.println(namesList);
+        System.out.println(" ");
         System.out.println(createNoRepetitionList(list1));
-        System.out.println(getReverseOrderList(list2));
+        System.out.println(" ");
+        getReverseOrderList(list2);
+        System.out.println(list2);
+        System.out.println(" ");
+
         modifyListObjects(list3, "Anna", "Joanna");
         System.out.println(list3);
     }
 
-    static List createNoRepetitionList(List list) {
-        for (int i = 0; i < list.size(); i++ ) {
-            for (int j = i+1; j < list.size(); j++) {
-                if (list.get(i).equals(list.get(j))) {
-                    list.remove(list.get(j));
-                }
-            }
-        }
-        return list;
+    static Set<String> createNoRepetitionList(List<String> list) {
+        Set<String> noRepList = new HashSet<>();
+        noRepList.addAll(list);
+        return noRepList;
     }
 
-    static List getReverseOrderList (List list) {
-        var reverse = new ArrayList<>();
-        for (int i = list.size()-1; i >= 0; i--) {
-            reverse.add(list.get(i));
-        }
-        return reverse;
+    static void getReverseOrderList (List<String> list) {
+        Collections.reverse(list);
     }
 
     static void modifyListObjects(List c, String renameFrom, String renameTo) {
-        for (int i = 0; i < c.size(); i++) {
-            if (c.contains(renameFrom)) {
-                c.set(c.indexOf(renameFrom), renameTo);
-            }
-        }
+        Collections.replaceAll(c, renameFrom, renameTo);
     }
 }
